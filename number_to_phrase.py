f6c32326254554761f38525b335eede4d3ba270d
Cody Brunner <rockchalkwushock@icloud.com>
2018-06-21 11:26:32 -0700

    adds num-to-phrase

---
+++ number_to_phrase.py
@@ -0,0 +1,218 @@
+
+def num_to_phrase(num):
+    if len(num) == 2:
+        if num[0] == '1':
+            if num[1] == '0':
+                print('Ten')
+            elif num[1] == '1':
+                print('Eleven')
+            elif num[1] == '2':
+                print('Twelve')
+            elif num[1] == '3':
+                print('Thirteen')
+            elif num[1] == '4':
+                print('Fourteen')
+            elif num[1] == '5':
+                print('Fifteen')
+            elif num[1] == '6':
+                print('Sixteen')
+            elif num[1] == '7':
+                print('Seventeen')
+            elif num[1] == '8':
+                print('Eighteen')
+            elif num[1] == '9':
+                print('Nineteen')
+        elif num[0] == '2':
+            if num[1] == '0':
+                print('Twenty')
+            elif num[1] == '1':
+                print('Twenty-One')
+            elif num[1] == '2':
+                print('Twenty-Two')
+            elif num[1] == '3':
+                print('Twenty-Three')
+            elif num[1] == '4':
+                print('Twenty-Four')
+            elif num[1] == '5':
+                print('Twenty-Five')
+            elif num[1] == '6':
+                print('Twenty-Six')
+            elif num[1] == '7':
+                print('Twenty-Seven')
+            elif num[1] == '8':
+                print('Twenty-Eight')
+            elif num[1] == '9':
+                print('Twenty-Nine')
+        elif num[0] == '3':
+            if num[1] == '0':
+                print('Thirty')
+            elif num[1] == '1':
+                print('Thirty-One')
+            elif num[1] == '2':
+                print('Thirty-Two')
+            elif num[1] == '3':
+                print('Thirty-Three')
+            elif num[1] == '4':
+                print('Thirty-Four')
+            elif num[1] == '5':
+                print('Thirty-Five')
+            elif num[1] == '6':
+                print('Thirty-Six')
+            elif num[1] == '7':
+                print('Thirty-Seven')
+            elif num[1] == '8':
+                print('Thirty-Eight')
+            elif num[1] == '9':
+                print('Thirty-Nine')
+        elif num[0] == '4':
+            if num[1] == '0':
+                print('Fourty')
+            elif num[1] == '1':
+                print('Fourty-One')
+            elif num[1] == '2':
+                print('Fourty-Two')
+            elif num[1] == '3':
+                print('Fourty-Three')
+            elif num[1] == '4':
+                print('Fourty-Four')
+            elif num[1] == '5':
+                print('Fourty-Five')
+            elif num[1] == '6':
+                print('Fourty-Six')
+            elif num[1] == '7':
+                print('Fourty-Seven')
+            elif num[1] == '8':
+                print('Fourty-Eight')
+            elif num[1] == '9':
+                print('Fourty-Nine')
+        elif num[0] == '5':
+            if num[1] == '0':
+                print('Fifty')
+            elif num[1] == '1':
+                print('Fifty-One')
+            elif num[1] == '2':
+                print('Fifty-Two')
+            elif num[1] == '3':
+                print('Fifty-Three')
+            elif num[1] == '4':
+                print('Fifty-Four')
+            elif num[1] == '5':
+                print('Fifty-Five')
+            elif num[1] == '6':
+                print('Fifty-Six')
+            elif num[1] == '7':
+                print('Fifty-Seven')
+            elif num[1] == '8':
+                print('Fifty-Eight')
+            elif num[1] == '9':
+                print('Fifty-Nine')
+        elif num[0] == '6':
+            if num[1] == '0':
+                print('Sixty')
+            elif num[1] == '1':
+                print('Sixty-One')
+            elif num[1] == '2':
+                print('Sixty-Two')
+            elif num[1] == '3':
+                print('Sixty-Three')
+            elif num[1] == '4':
+                print('Sixty-Four')
+            elif num[1] == '5':
+                print('Sixty-Five')
+            elif num[1] == '6':
+                print('Sixty-Six')
+            elif num[1] == '7':
+                print('Sixty-Seven')
+            elif num[1] == '8':
+                print('Sixty-Eight')
+            elif num[1] == '9':
+                print('Sixty-Nine')
+        elif num[0] == '7':
+            if num[1] == '0':
+                print('Seventy')
+            elif num[1] == '1':
+                print('Seventy-One')
+            elif num[1] == '2':
+                print('Seventy-Two')
+            elif num[1] == '3':
+                print('Seventy-Three')
+            elif num[1] == '4':
+                print('Seventy-Four')
+            elif num[1] == '5':
+                print('Seventy-Five')
+            elif num[1] == '6':
+                print('Seventy-Six')
+            elif num[1] == '7':
+                print('Seventy-Seven')
+            elif num[1] == '8':
+                print('Seventy-Eight')
+            elif num[1] == '9':
+                print('Seventy-Nine')
+        elif num[0] == '8':
+            if num[1] == '0':
+                print('Eighty')
+            elif num[1] == '1':
+                print('Eighty-One')
+            elif num[1] == '2':
+                print('Eighty-Two')
+            elif num[1] == '3':
+                print('Eighty-Three')
+            elif num[1] == '4':
+                print('Eighty-Four')
+            elif num[1] == '5':
+                print('Eighty-Five')
+            elif num[1] == '6':
+                print('Eighty-Six')
+            elif num[1] == '7':
+                print('Eighty-Seven')
+            elif num[1] == '8':
+                print('Eighty-Eight')
+            elif num[1] == '9':
+                print('Eighty-Nine')
+        elif num[0] == '9':
+            if num[1] == '0':
+                print('Ninety')
+            elif num[1] == '1':
+                print('Ninety-One')
+            elif num[1] == '2':
+                print('Ninety-Two')
+            elif num[1] == '3':
+                print('Ninety-Three')
+            elif num[1] == '4':
+                print('Ninety-Four')
+            elif num[1] == '5':
+                print('Ninety-Five')
+            elif num[1] == '6':
+                print('Ninety-Six')
+            elif num[1] == '7':
+                print('Ninety-Seven')
+            elif num[1] == '8':
+                print('Ninety-Eight')
+            elif num[1] == '9':
+                print('Ninety-Nine')
+    else:
+        if num[0] == '0':
+            print('Zero')
+        elif num[0] == '1':
+            print('One')
+        elif num[0] == '2':
+            print('Two')
+        elif num[0] == '3':
+            print('Three')
+        elif num[0] == '4':
+            print('Four')
+        elif num[0] == '5':
+            print('Five')
+        elif num[0] == '6':
+            print('Six')
+        elif num[0] == '7':
+            print('Seven')
+        elif num[0] == '8':
+            print('Eight')
+        elif num[0] == '9':
+            print('Nine')
+
+
+num = list(input('Enter a number (0-99): '))
+
+num_to_phrase(num)
