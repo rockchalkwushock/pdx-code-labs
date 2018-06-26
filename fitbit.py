f6d4224c27e88d40b647c3626e54275eb859e93f
Cody Brunner <rockchalkwushock@icloud.com>
2018-06-26 13:42:13 -0700

    adds fitbit

---
+++ fitbit.py
@@ -0,0 +1,61 @@
+import requests
+
+DATE = '2018-06-26'
+TOKEN = ''
+USER_ID = ''
+
+headers = {
+    "authorization": 'Bearer {TOKEN}'
+}
+
+r = requests.get(
+    'https://api.fitbit.com/1/user/{USER_ID}/activities/date/{DATE}.json', headers=headers)
+
+print(r.url)
+print(r.status_code)
+response = r.json()
+steps = response['summary']['steps']
+fat_burn = response['summary']['heartRateZones'][1]['caloriesOut']
+
+print(
+    f'Current Steps: {steps}.\nCalories burned in Fat Burn zone: {round(fat_burn, 2)}.')
+
+# {'activities': [],
+#  'goals': {
+#     'activeMinutes': 30,
+#     'caloriesOut': 2978,
+#     'distance': 8.05,
+#     'floors': 10,
+#     'steps': 10000
+# },
+#     'summary': {
+#     'activeScore': -1,
+#     'activityCalories': 198,
+#     'caloriesBMR': 946,
+#     'caloriesOut': 1106,
+#     'distances': [
+#         {'activity': 'total', 'distance': 1.22},
+#         {'activity': 'tracker', 'distance': 1.22},
+#         {'activity': 'loggedActivities', 'distance': 0},
+#         {'activity': 'veryActive', 'distance': 0},
+#         {'activity': 'moderatelyActive', 'distance': 0},
+#         {'activity': 'lightlyActive', 'distance': 1.22},
+#         {'activity': 'sedentaryActive', 'distance': 0}
+#     ],
+#     'elevation': 12.19,
+#     'fairlyActiveMinutes': 0,
+#     'floors': 4,
+#     'heartRateZones': [
+#         {'caloriesOut': 479.99493, 'max': 95, 'min': 30, 'minutes': 303, 'name': 'Out of Range'},
+#         {'caloriesOut': 52.49004, 'max': 133, 'min': 95, 'minutes': 8, 'name': 'Fat Burn'},
+#         {'caloriesOut': 0, 'max': 161, 'min': 133, 'minutes': 0, 'name': 'Cardio'},
+#         {'caloriesOut': 0, 'max': 220, 'min': 161, 'minutes': 0, 'name': 'Peak'}
+#     ],
+#     'lightlyActiveMinutes': 41,
+#     'marginalCalories': 118,
+#     'restingHeartRate': 60,
+#     'sedentaryMinutes': 745,
+#     'steps': 1656,
+#     'veryActiveMinutes': 0
+# }
+# }
