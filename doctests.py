6e6f8a5af96328194ec253f3c7b3a16ff191c6ad
Cody Brunner <rockchalkwushock@icloud.com>
2018-07-11 12:33:08 -0700

    adds doctests example

---
+++ doctests.py
@@ -0,0 +1,12 @@
+def knights():
+    """
+    Returns a string "Knights who say Ni!".
+    >>> knights()
+    'Knights who say Ni!'
+    """
+    return 'Knights who say Ni!'
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
