92805208fd00b6094f591676ac1138f08fe66b03
Cody Brunner <rockchalkwushock@icloud.com>
2018-08-20 14:03:13 -0700

    adds djrf tut

---
+++ django-rest-framework-tutorial/tutorial/snippets/views.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User  # new
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view  # new
+from rest_framework.response import Response  # new
+from rest_framework.reverse import reverse  # new
+
+from .models import Snippet
+from .permissions import IsOwnerOrReadOnly
+from .serializers import SnippetSerializer, UserSerializer  # new
+
+
+@api_view(['GET'])  # new
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):  # new
+        serializer.save(owner=self.request.user)
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+
+class SnippetHighlight(generics.GenericAPIView):  # new
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+class UserList(generics.ListAPIView):  # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):  # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
