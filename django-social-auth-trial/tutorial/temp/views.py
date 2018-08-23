9953fda1c1b727c98834d5b10ea6a742967af470
Cody Brunner <rockchalkwushock@icloud.com>
2018-08-23 10:15:18 -0700

    adds grabbing the token

--- django-social-auth-trial/tutorial/temp/views.py
+++ django-social-auth-trial/tutorial/temp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def index(request):
     print(request.user)
-
+    if request.user.is_authenticated:
+        social = request.user.social_auth.get(provider='fitbit')
+        token = social.extra_data['access_token']
+        print(token)
     return render(request, 'temp/index.html')
 
 # https://www.fitbit.com/oauth2/authorize?client_id=22D3H4&redirect_uri=http://localhost:8000/complete/fitbit/&state=dqOuEqrXhT7nxrTyxK0fpGvgOklUQDo2&response_type=code&scope=activity+heartrate+profile+sleep+weight+profile
